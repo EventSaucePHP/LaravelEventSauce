58fee81068dc8bc5c77200318b121491f096f318
freek <freek@spatie.be>
2019-01-15 20:14:30 +0100

    fixes

--- src/EventSauceServiceProvider.php
+++ src/EventSauceServiceProvider.php
@@ -13,8 +13,30 @@ use Illuminate\Support\ServiceProvider;
 
 class EventSauceServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/eventsauce.php' => config_path('eventsauce.php'),
+            ], 'config');
+        }
+        if (!class_exists('CreateDomainMessagesTable')) {
+            $this->publishes([
+                __DIR__ . '/../migrations/create_domain_messages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_domain_messages_table.php'),
+            ], 'migrations');
+        }
+    }
+
     public function register()
     {
+        $this->commands([
+            GenerateCodeCommand::class,
+        ]);
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/eventsauce.php', 'eventsauce'
+        );
+
         $this->app->bind(AggregateRootRepository::class, function (Container $app) {
             return new ConstructingAggregateRootRepository(
                 $app['config']->get('eventsauce.aggregate_root'),
@@ -51,5 +73,12 @@ class EventSauceServiceProvider extends ServiceProvider
 
             return $job->handle($dispatcher);
         });
+    }
+
+    public function provides()
+    {
+        return [
+            GenerateCodeCommand::class,
+        ];
     }
 }
--- src/GenerateCodeCommand.php
+++ src/GenerateCodeCommand.php
@@ -6,26 +6,37 @@ use EventSauce\EventSourcing\CodeGeneration\CodeDumper;
 use EventSauce\EventSourcing\CodeGeneration\DefinitionGroup;
 use EventSauce\EventSourcing\CodeGeneration\YamlDefinitionLoader;
 use Illuminate\Console\Command;
-use function file_put_contents;
 
 final class GenerateCodeCommand extends Command
 {
     protected $signature = 'eventsauce:generate-code';
-    protected $description = 'Generate EventSauce Code.';
+    
+    protected $description = 'Generate EventSauce code.';
 
     public function handle()
     {
-        $definitionPath = (array) config('eventsauce.definition');
-        $outputPath = config('eventsauce.output');
+        $this->info('Start generating code...');
+
+        $codeGenerationConfig = data_get(config('eventsauce'), 'aggregate_roots.*.code_generation');
+
+        collect($codeGenerationConfig)->each(function(array $config) {
+            $this->generateCode($config['definition_file'], $config['output_to_file']);
+        });
+
+        $this->info('All done!');
+    }
+
+    private function generateCode(string $definitionFile, string $outputFile)
+    {
         $loader = new YamlDefinitionLoader();
-        $definitionGroup = new DefinitionGroup();
         $dumper = new CodeDumper();
 
-        foreach ($definitionPath as $path) {
-            $definitionGroup = $loader->load($path, $definitionGroup);
-        }
+        $loadedDefinitionFile = $loader->load($definitionFile);
 
-        file_put_contents($outputPath, $dumper->dump($definitionGroup));
-        $this->output->writeln('Code generated!');
+        $phpCode = $dumper->dump($loadedDefinitionFile);
+
+        file_put_contents($outputFile, $phpCode);
+
+        $this->warn("Written code to `{$outputFile}`");
     }
 }
