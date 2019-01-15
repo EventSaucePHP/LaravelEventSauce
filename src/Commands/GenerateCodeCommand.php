98d71639932e9e35d484aaa899a002dd5cdd738f
freek <freek@spatie.be>
2019-01-15 22:12:09 +0100

    look momma, I am strict now

--- src/Commands/GenerateCodeCommand.php
+++ src/Commands/GenerateCodeCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EventSauce\LaravelEventSauce\Commands;
 
 use EventSauce\EventSourcing\CodeGeneration\CodeDumper;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EventSauce\LaravelEventSauce\Tests;
 
 use EventSauce\LaravelEventSauce\EventSauceServiceProvider;
