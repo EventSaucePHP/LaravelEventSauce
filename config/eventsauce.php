41f586bd09d6878d87ef78d193063ade96000376
freek <freek@spatie.be>
2019-01-16 08:31:04 +0100

    wip

--- config/eventsauce.php
+++ config/eventsauce.php
@@ -2,47 +2,46 @@
 
 return [
     'aggregate_roots' => [
-         [
-             /*
-              * An aggregate root is an entity that is modelled using events.
-              *
-              * See: https://eventsauce.io/docs/getting-started/create-an-aggregate-root/
-              */
-            'aggregate_root' => null,
+        /*
+         * An aggregate root is an entity that is modelled using events.
+         *
+         * See: https://eventsauce.io/docs/getting-started/create-an-aggregate-root/
+         */
+        'aggregate_root' => null,
 
-             /*
-              * TODO
-              */
-            'repository' => null,
+        /*
+         * TODO
+         */
+        'repository' => null,
 
 
-             /*
-              * Consumers are classes that listen to events and do something with them, for
-              * example projecting data.
-              */
-            'sync_consumers' => [
-                // ...
-            ],
+        /*
+         * Consumers are classes that listen to events and do something with them, for
+         * example projecting data.
+         */
+        'sync_consumers' => [
+            // ...
+        ],
 
-             /*
-              * These consumers will be executing in a queued job.
-              */
-            'async_consumers' => [
-                // ...
-            ],
+        /*
+         * These consumers will be executing in a queued job.
+         */
+        'async_consumers' => [
+            // ...
+        ],
 
-            /*
-             * We can generate  types, commands and events for you starting from a yaml file.
-             * Here you can specify the input and the output.
-             *
-             * More info on code generation here: https://eventsauce.io/docs/getting-started/create-events-and-commands
-             */
-            'code_generation' => [
-                'input_yaml_file' => null,
-                'output_file' => null,
-            ],
+        /*
+         * We can generate  types, commands and events for you starting from a yaml file.
+         * Here you can specify the input and the output.
+         *
+         * More info on code generation here: https://eventsauce.io/docs/getting-started/create-events-and-commands
+         */
+        'code_generation' => [
+            'input_yaml_file' => null,
+            'output_file' => null,
         ],
     ],
+
 
     /*
      * This class is responsible for dispatching jobs.
