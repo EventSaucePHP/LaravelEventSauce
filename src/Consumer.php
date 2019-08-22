124222fc6200177564d14b459419dd95c9ab6c96
Dries Vints <dries.vints@gmail.com>
2019-08-22 21:15:53 +0200

    Consistent naming

--- src/Consumer.php
+++ src/Consumer.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace EventSauce\LaravelEventSauce;
 
-use EventSauce\EventSourcing\Consumer as ConsumerContract;
+use EventSauce\EventSourcing\Consumer as EventSauceConsumer;
 use EventSauce\EventSourcing\Message;
 
-abstract class Consumer implements ConsumerContract
+abstract class Consumer implements EventSauceConsumer
 {
     public function handle(Message $message): void
     {
