9896ec59f58b0cc6f0fc3bb9c000d020605955b8
Sander van Hooft <info@sandervanhooft.nl>
2019-10-25 23:58:44 +0200

    Use existing fixtures

--- tests/TestTools/AggregateRootTestCaseTest.php
+++ tests/TestTools/AggregateRootTestCaseTest.php
@@ -4,18 +4,19 @@ namespace Tests\TestTools;
 
 use EventSauce\EventSourcing\AggregateRootId;
 use EventSauce\LaravelEventSauce\TestTools\AggregateRootTestCase;
-use Illuminate\Support\Str;
+use Tests\Fixtures\RegistrationAggregateRoot;
+use Tests\Fixtures\RegistrationAggregateRootId;
 
 class AggregateRootTestCaseTest extends AggregateRootTestCase
 {
     protected function newAggregateRootId(): AggregateRootId
     {
-        return SignupId::create();
+        return RegistrationAggregateRootId::create();
     }
 
     protected function aggregateRootClassName(): string
     {
-        return SignupProcess::class;
+        return RegistrationAggregateRoot::class;
     }
 
     /** @test */
@@ -23,42 +24,4 @@ class AggregateRootTestCaseTest extends AggregateRootTestCase
     {
         $this->assertTrue(true);
     }
-}
-
-class SignupId implements AggregateRootId
-{
-    private $id;
-
-    public function __construct($id)
-    {
-        $this->id = $id;
-    }
-
-    public static function create()
-    {
-        return new static(Str::uuid());
-    }
-
-    /**
-     * @return string
-     */
-    public function toString(): string
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param string $aggregateRootId
-     *
-     * @return static
-     */
-    public static function fromString(string $aggregateRootId): AggregateRootId
-    {
-        return new static($aggregateRootId);
-    }
-}
-
-class SignupProcess
-{
-    //
 }
