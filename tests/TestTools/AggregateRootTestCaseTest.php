47da5c9a9416774f1e62df234ace8d20fff1202d
Sander van Hooft <info@sandervanhooft.nl>
2019-10-26 00:18:58 +0200

    Then, when and thenNothingShouldHaveHappened work

--- tests/TestTools/AggregateRootTestCaseTest.php
+++ tests/TestTools/AggregateRootTestCaseTest.php
@@ -4,8 +4,10 @@ namespace Tests\TestTools;
 
 use EventSauce\EventSourcing\AggregateRootId;
 use EventSauce\LaravelEventSauce\TestTools\AggregateRootTestCase;
+use Tests\Fixtures\RegisterUser;
 use Tests\Fixtures\RegistrationAggregateRoot;
 use Tests\Fixtures\RegistrationAggregateRootId;
+use Tests\Fixtures\UserWasRegistered;
 
 class AggregateRootTestCaseTest extends AggregateRootTestCase
 {
@@ -19,9 +21,33 @@ class AggregateRootTestCaseTest extends AggregateRootTestCase
         return RegistrationAggregateRoot::class;
     }
 
+    public function handle(object $command)
+    {
+        if($command instanceof RegisterUser) {
+            /** @var RegistrationAggregateRoot $aggregate */
+            $aggregate = $this->repository->retrieve($this->aggregateRootId);
+            $aggregate->registerUser($command);
+            $this->repository->persist($aggregate);
+        }
+    }
+
     /** @test */
     public function itInitializes()
     {
         $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function whenThenWorks()
+    {
+        $this->when(new RegisterUser('Foo', 'john@example.com'))
+             ->then(new UserWasRegistered('Foo', 'john@example.com'));
+    }
+
+    /** @test */
+    public function whenThenNothingShouldHaveHappenedWorks()
+    {
+        $this->when(new UserWasRegistered('Foo', 'john@example.com'))
+            ->thenNothingShouldHaveHappened();
     }
 }
