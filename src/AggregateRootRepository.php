324119ba9f8d30af099da49a24ac9ec1e545b8bc
Deon van der Vyver <deonvdv@gmail.com>
2019-11-22 17:01:12 +0100

    Add support for MessageDecorators

--- src/AggregateRootRepository.php
+++ src/AggregateRootRepository.php
@@ -9,84 +9,101 @@ use EventSauce\EventSourcing\AggregateRootId;
 use EventSauce\EventSourcing\AggregateRootRepository as EventSauceAggregateRootRepository;
 use EventSauce\EventSourcing\ConstructingAggregateRootRepository;
 use EventSauce\EventSourcing\MessageDispatcherChain;
+use EventSauce\EventSourcing\MessageDecorator;
+use EventSauce\EventSourcing\MessageDecoratorChain;
+use Generator;
 use LogicException;
 
 abstract class AggregateRootRepository implements EventSauceAggregateRootRepository
 {
-    /** @var LaravelMessageRepository */
-    private $messageRepository;
+	/** @var LaravelMessageRepository */
+	private $messageRepository;
 
-    /** @var string */
-    protected $aggregateRoot;
+	/** @var string */
+	protected $aggregateRoot;
 
-    /** @var array */
-    protected $consumers = [];
+	/** @var array */
+	protected $consumers = [];
 
-    /** @var string */
-    protected $connection;
+	/** @var array */
+	protected $decorators = [];
 
-    /** @var string */
-    protected $table;
+	/** @var string */
+	protected $connection;
 
-    /** @var string */
-    protected static $inputFile = '';
+	/** @var string */
+	protected $table;
 
-    /** @var string */
-    protected static $outputFile = '';
+	/** @var string */
+	protected static $inputFile = '';
 
-    public function __construct(LaravelMessageRepository $messageRepository)
-    {
-        if ($this->aggregateRoot === null || ! is_a($this->aggregateRoot, AggregateRoot::class, true)) {
-            throw new LogicException('You have to set an aggregate root before the repository can be initialized.');
-        }
+	/** @var string */
+	protected static $outputFile = '';
 
-        $this->messageRepository = $messageRepository;
+	public function __construct(LaravelMessageRepository $messageRepository)
+	{
+		if ($this->aggregateRoot === null || !is_a($this->aggregateRoot, AggregateRoot::class, true)) {
+			throw new LogicException('You have to set an aggregate root before the repository can be initialized.');
+		}
 
-        if ($this->connection) {
-            $this->messageRepository->setConnection($this->connection);
-        }
+		$this->messageRepository = $messageRepository;
 
-        if ($this->table) {
-            $this->messageRepository->setTable($this->table);
-        }
-    }
+		if ($this->connection) {
+			$this->messageRepository->setConnection($this->connection);
+		}
 
-    public function retrieve(AggregateRootId $aggregateRootId): object
-    {
-        return $this->repository()->retrieve($aggregateRootId);
-    }
+		if ($this->table) {
+			$this->messageRepository->setTable($this->table);
+		}
+	}
 
-    public function persist(object $aggregateRoot)
-    {
-        $this->repository()->persist($aggregateRoot);
-    }
+	public function retrieve(AggregateRootId $aggregateRootId): object
+	{
+		return $this->repository()->retrieve($aggregateRootId);
+	}
 
-    public function persistEvents(AggregateRootId $aggregateRootId, int $aggregateRootVersion, object ...$events)
-    {
-        $this->repository()->persistEvents($aggregateRootId, $aggregateRootVersion, ...$events);
-    }
+	public function persist(object $aggregateRoot)
+	{
+		$this->repository()->persist($aggregateRoot);
+	}
 
-    private function repository(): EventSauceAggregateRootRepository
-    {
-        return new ConstructingAggregateRootRepository(
-            $this->aggregateRoot,
-            $this->messageRepository,
-            new MessageDispatcherChain(
-                new LaravelMessageDispatcher(
-                    ...$this->consumers
-                ),
-                new EventMessageDispatcher()
-            )
-        );
-    }
+	public function persistEvents(AggregateRootId $aggregateRootId, int $aggregateRootVersion, object ...$events)
+	{
+		$this->repository()->persistEvents($aggregateRootId, $aggregateRootVersion, ...$events);
+	}
 
-    public static function inputFile(): string
-    {
-        return static::$inputFile;
-    }
+	private function repository(): EventSauceAggregateRootRepository
+	{
+		return new ConstructingAggregateRootRepository(
+			$this->aggregateRoot,
+			$this->messageRepository,
+			new MessageDispatcherChain(
+				new LaravelMessageDispatcher(
+					...$this->consumers
+				),
+				new EventMessageDispatcher()
+			),
+			new MessageDecoratorChain(
+				...$this->instantiateDecorators()
+			),
+		);
+	}
 
-    public static function outputFile(): string
-    {
-        return static::$outputFile;
-    }
+	public static function inputFile(): string
+	{
+		return static::$inputFile;
+	}
+
+	public static function outputFile(): string
+	{
+		return static::$outputFile;
+	}
+
+	private function instantiateDecorators(): Generator
+	{
+		// Ensure
+		foreach ($this->decorators as $decorator) {
+			yield app($decorator);
+		}
+	}
 }
